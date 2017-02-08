a79d13a74544dc855017a10d2d1b92fda6c585f7
Katie Wood <woodkm11@gmail.com>
2017-02-08 12:55:33 -0500

    updated list show view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root "lists#index"
-  resources :books
-  resources :lists
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    root 'lists#index'
+
+    resources :books
+    resources :lists
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
