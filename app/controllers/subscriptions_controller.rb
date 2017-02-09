0676b2e81b7396da8b846045fdf7c886084e8b5c
Katie Wood <woodkm11@gmail.com>
2017-02-08 22:42:12 -0500

    subscribe path working

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -3,5 +3,6 @@ class SubscriptionsController < ApplicationController
         @user = current_user
         @list = List.find_by_id(params[:id])
         Subscription.create(user_id: @user.id, list_id: @list.id)
+        redirect_to lists_path
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     resources :books
     resources :lists
 
+    get '/lists/:id/subscribe', to: 'subscriptions#create', as: 'subscription'
+
     # post '/lists/:id', to: 'subscriptions#create', as: 'subscription'
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
