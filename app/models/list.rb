5fd223f76594b65b873ba5cc78f25b5821f32f58
Katie Wood <woodkm11@gmail.com>
2017-02-09 14:52:10 -0500

    updated list model

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,7 @@
 class List < ApplicationRecord
-    has_many :selections
+    has_many :selections, dependent: :destroy
     has_many :books, through: :selections
 
-    has_many :subscriptions
+    has_many :subscriptions, dependent: :destroy
     has_many :users, through: :subscriptions
 end
