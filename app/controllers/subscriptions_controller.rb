1d501af9d77940bc00baca33ee351701d1d19e2b
Katie Wood <woodkm11@gmail.com>
2017-02-10 13:28:30 -0500

    added unsubscribe functionality

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -5,4 +5,15 @@ class SubscriptionsController < ApplicationController
         Subscription.create(user_id: @user.id, list_id: @list.id)
         redirect_to lists_path
     end
+
+    def index
+        @user = current_user
+        @subscription = @user.subscriptions.all
+    end
+
+    def destroy
+        @user = current_user
+        Subscription.find(params[:id]).destroy
+        redirect_to subscriptions_path
+    end
 end
